dd2de587077bc65148f7b7085ac49d7745c15a9a
Samuel Mortenson <samuel.mortenson@acquia.com>
2018-03-30 13:23:17 -0700

    Lazy-load twig components in CKEditor.

--- js/twig_components_ckeditor.js
+++ js/twig_components_ckeditor.js
@@ -3,40 +3,46 @@
  * Ensures that twig components are properly loaded into CKEditor.
  */
 
-(function (Drupal, CKEDITOR) {
+(function (settings, CKEDITOR) {
 
   "use strict";
 
-  /**
-   * Behaviors related to CKEditor.
-   */
-  Drupal.behaviors.twigComponentCKEditor = {
-    attach: function (context, settings) {
-      for (var i in CKEDITOR.instances) {
-        if (CKEDITOR.instances[i].twigComponentsProcessed) {
-          continue;
-        }
-        else {
-          CKEDITOR.instances[i].twigComponentsProcessed = true;
-        }
-        CKEDITOR.instances[i].on('contentDom', function () {
-          var iframeDoc = CKEDITOR.instances[i].document.$;
-          var wrapper = iframeDoc.createElement('div');
-          wrapper.innerHTML = settings.twigComponentsCKEditorScripts;
-          [].forEach.call(wrapper.childNodes, function (child) {
-            if (child.tagName === 'SCRIPT') {
+  function attachScripts () {
+    if (!settings.twigComponentsCKEditorScripts) {
+      return;
+    }
+    var iframeDoc = this.document.$;
+    for (var tag_name in settings.twigComponentsCKEditorScripts) {
+      if (iframeDoc.getElementsByTagName(tag_name).length) {
+        var wrapper = iframeDoc.createElement('div');
+        wrapper.innerHTML = settings.twigComponentsCKEditorScripts[tag_name];
+        [].forEach.call(wrapper.childNodes, function (child) {
+          if (child.tagName === 'SCRIPT') {
+            if (child.src && !iframeDoc.querySelectorAll('[src="' + child.src + '"]').length) {
               var script = iframeDoc.createElement('script');
               script.src = child.src;
               script.async = false;
               iframeDoc.head.appendChild(script);
             }
-            else {
-              iframeDoc.head.appendChild(child.cloneNode());
-            }
-          });
+          }
+          else {
+            iframeDoc.head.appendChild(child.cloneNode());
+          }
         });
       }
     }
-  };
+  }
 
-}(Drupal, CKEDITOR));
+  CKEDITOR.on('instanceReady', function (event) {
+    var editor = event.editor;
+    if (editor.twigComponentsProcessed) {
+      return;
+    }
+    else {
+      editor.twigComponentsProcessed = true;
+    }
+    editor.on('change', attachScripts);
+    attachScripts.call(editor);
+  });
+
+}(drupalSettings, CKEDITOR));
